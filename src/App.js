f5a657207bda306b287bf8481aeac4c7b58508dc
masiavinash <masiavinash013@gmail.com>
2025-03-28 11:36:47 +0530

    zdes

--- src/App.js
+++ src/App.js
@@ -1,44 +1,22 @@
-
-
-import "./App.css";
-import { BrowserRouter, Route, Routes,Link } from "react-router-dom";
-import App1 from "./components/App1";
-import App2 from "./components/App2";
-import App3 from "./components/App3";
-import App4 from "./components/App4";
-import App5 from "./components/App5";
-import App6 from "./components/App6";
-import App7 from "./components/App7";
-import App8 from "./components/App8";
-function App() {
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Cart from "./components/Cart";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Products from "./components/Products";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function App(props) {
   return (
     <BrowserRouter>
-      <div>
-        <div>
-          <Link to="app1" className="box">App1</Link>
-          <Link to="app2" className="box">App2</Link>
-          <Link to="app3" className="box">App3</Link>
-          <Link to="app4" className="box">App4</Link>
-          <Link to="app5" className="box">App5</Link>
-          <Link to="app6" className="box">App6</Link>
-          <Link to="app7" className="box">App7</Link>
-          <Link to="app8" className="box">App8</Link>
-        </div>
-        <hr></hr>
-        <div>
-          <Routes>
-            <Route index element={<App1 />} />
-            <Route path="app1" element={<App1 />} />
-            <Route path="app2" element={<App2 />} />
-            <Route path="app3" element={<App3 />} />
-            <Route path="app4" element={<App4 />} />
-            <Route path="app5" element={<App5 />} />
-            <Route path="app6" element={<App6 />} />
-            <Route path="app7" element={<App7 />} />
-            <Route path="app8" element={<App8 />} />
-          </Routes>
-        </div>
-      </div>
+      <Header />
+      <Routes>
+        <Route index element={<Products />} />
+        <Route path="home" element={<Products />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
